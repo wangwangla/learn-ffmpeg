72cfdfa59365979bad0ea0338247acceef8ec1f4
wangwangla <2818815189@qq.com>
2021-04-10 23:41:44 +0800

    初始化

---
+++ app/src/main/cpp/native-lib.cpp
@@ -0,0 +1,13 @@
+#include <jni.h>
+#include <string>
+
+extern "C"{
+#include <libavcodec/avcodec.h>
+}
+extern "C" JNIEXPORT jstring JNICALL
+Java_com_kangwang_ffmpeddemo_MainActivity_stringFromJNI(
+        JNIEnv* env,
+        jobject /* this */) {
+    std::string hello = "Hello from C++";
+    return env->NewStringUTF(av_version_info());
+}
